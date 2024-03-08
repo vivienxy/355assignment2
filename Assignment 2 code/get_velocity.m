0d32aad69f0ae46391af66fbaf4b9446927ace5a
hails <y366kim@uwaterloo.ca>
2024-03-08 18:48:36 -0500

    Part 2

--- Assignment 2 code/get_velocity.m
+++ Assignment 2 code/get_velocity.m
@@ -3,7 +3,7 @@ function [root] = get_velocity(a, lm, lt)
 % Input Parameters
 % a: activation (between 0 and 1)
 % lm: normalized length of muscle (contractile element)
-%  lt: normalized length of tendon (series elastic element)
+% lt: normalized length of tendon (series elastic element)
 
 % Output
 % root: normalized lengthening velocity of muscle (contractile element)
@@ -13,7 +13,19 @@ function [root] = get_velocity(a, lm, lt)
 beta = 0.1;
 
 % WRITE CODE HERE TO CALCULATE VELOCITY
+alpha = 0;
+a = 1;
+lm = 1;
+lt = 1.01;
+Ft = force_length_tendon(lt);
+Fpe = force_length_parallel(lm);
+Fl = get_muscle_force_length_regression();
+Fv = get_muscle_force_velocity_regression();
 
+vm_func = (Ft - Fpe - (a * Fl * Fv)) / beta * cos(alpha);
+x0 = 0;
+
+[root] = fzero(vm_func, x0, options);
 
 end
 
