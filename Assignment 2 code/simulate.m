a0816e5bedccead0ce71f48d3413d6e7bd6358d9
Vivien <v9yu@uwaterloo.ca>
2024-03-08 20:32:53 -0500

    part 3

--- Assignment 2 code/simulate.m
+++ Assignment 2 code/simulate.m
@@ -6,16 +6,48 @@ function simulate(T, f0M, resting_length_muscle, resting_length_tendon, RelTol, 
 % resting_length_muscle: actual length (m) of muscle (CE) that corresponds to normalized length of 1
 % resting_length_tendon: actual length of tendon (m) that corresponds to normalized length of 1
 
-%%% TASK 1
+%%% TASK 1 
+% created instance of class and assigned variables 
 
-%%% TASK 2
+h = HillTypeMuscle; 
+h.resting_length_muscle = resting_length_muscle;
+h.resting_length_tendon = resting_length_tendon;
+h.f0M = f0M;
 
+%%% TASK 2 
 
-%%% TASK 3 xdot = vm
+    function a = activate(T)
+        if T < 0.5
+            a = 0;
+        else 
+            a = 1;
+        end
+    end
+
+    function dx1dt = vm_fct(time, norm_lm)
+        x1 = norm_lm;
+        dx1dt = get_velocity(activate(time), x1, h.norm_tendon_length(x1));
+    end
+
+%%% TASK 3 
+% dx1dt = vm_fct
 % the outputs of ode45 must be named "time" and "norm_lm"
 
-%%% TASK 4
+x1_0 = 1; % initial condition 
+tspan = [0 T]; % simulation time
+
+rt = RelTol;
+at = AbsTol;
+
+options = odeset('RelTol',rt,'AbsTol',at);
+
+[time, norm_lm] = ode45(@vm_fct, tspan, x1_0, options);
+
+%%% TASK 4 
 % save the estimated forces in a vector called "forces"
+
+total_length = h.resting_length_muscle + h.resting_length_tendon;
+forces = h.get_force(total_length, norm_lm);
 
 %%%% Do not alter the rest (it should not be needed) %%%%%%
 norm_lm = norm_lm * resting_length_muscle;
